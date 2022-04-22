6fd6bd7d526460016e4dc7338e8d30fde9cd1789
M00ntasi-R <bijoymoontasir@gmail.com>
2022-04-22 10:39:21 +0600

    v3

--- script.js
+++ script.js
@@ -61,3 +61,46 @@ function getMealByID(mealID) {
       });
   }
   
+//add meal to dom
+function addMealToDom(meal) {
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      if (meal[`strIngredient${i}`]) {
+        ingredients.push(
+          `${meal[`strIngredient${i}`]} = ${meal[`strMeasure${i}`]}`
+        );
+      } else {
+        break;
+      }
+    }
+    single_mealEl.innerHTML = `<div class="single-meal">
+          <h1>${meal.strMeal}</h1>
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+          <div class="single-meal-info">
+              ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+              ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+          </div>
+          <div class="main">
+              <p>${meal.strInstructions}</p>
+              <h2>Ingredients</h2>
+              <ul>
+                  ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+              </ul>
+          </div>
+      </div>`;
+  }
+  submit.addEventListener("submit", searchMeal);
+  random.addEventListener("click", randomMeal);
+  mealsEl.addEventListener("click", (e) => {
+    const mealInfo = e.path.find((item) => {
+      if (item.classList) {
+        return item.classList.contains("meal-info");
+      } else {
+        return false;
+      }
+    });
+    if (mealInfo) {
+      const mealID = mealInfo.getAttribute("data-mealid");
+      getMealByID(mealID);
+    }
+  });
