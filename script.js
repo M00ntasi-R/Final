99997e675b17c68e75436abc9a897e769a79962b
M00ntasi-R <bijoymoontasir@gmail.com>
2022-04-22 10:33:49 +0600

    v2

---
+++ script.js
@@ -0,0 +1,41 @@
+const search = document.getElementById("search"),
+  submit = document.getElementById("submit"),
+  random = document.getElementById("random"),
+  mealsEl = document.getElementById("meals"),
+  resultHeading = document.getElementById("result-heading"),
+  single_mealEl = document.getElementById("single-meal");
+
+function searchMeal(e) {
+  e.preventDefault();
+  //clear single meal
+  single_mealEl.innerHTML = "";
+  //get search term
+  const term = search.value;
+  //check for empty
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        resultHeading.innerHTML = `<h2>Search result of '${term}'</h2>`;
+        if (data.meals === null) {
+          resultHeading.innerHTML = `There is no result.Please try again!`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) =>
+                `<div class="meal">
+                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                           <h3>${meal.strMeal}</h3> 
+                        </div>
+                    </div>`
+            )
+            .join("");
+        }
+      });
+    //clear search text
+    search.value = "";
+  } else {
+    resultHeading.innerHTML = "<h2>Please type in the keywords to search</h2>";
+  }
+}
